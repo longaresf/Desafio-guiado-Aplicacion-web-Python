a17badb25cb9d85b1e85b028481ecb095fb04501
Francisco Longares <francisco_longares@yahoo.com>
2024-06-05 17:19:18 -0400

    make it better

---
+++ web/web_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.template import Template, Context, loader
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
